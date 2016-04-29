600a8b0c8348ee15b107ca310712f19489f5d512
Prelang Builder <builder@prelang.com>
2016-04-29 18:27:14 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Previsao' to allow for create, update

--- app/admin/previsao.rb
+++ app/admin/previsao.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Previsao do
+  permit_params :previsao, :ativo
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
