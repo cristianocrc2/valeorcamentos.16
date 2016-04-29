6b4caa776adcb1dd37c005e20c125b9ee0efdff6
Prelang Builder <builder@prelang.com>
2016-04-29 18:27:21 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Subcategoria' to allow for create, update

--- app/admin/subcategoria.rb
+++ app/admin/subcategoria.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Subcategoria do
+  permit_params :subcategoria, :ativo
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
